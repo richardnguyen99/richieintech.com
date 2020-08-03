80690c6e7ac2851f78aaf0a532169de3addc92f2
richardnguyen99 <richard.ng0616@gmail.com>
2020-08-03 16:58:02 -0700

    Set up Testing with Jest

---
+++ src/components/__tests__/Layout.tsx
@@ -0,0 +1,17 @@
+/**
+ * Testing file for Layout component
+ *
+ * @author Richard Nguyen <richard.ng0616@gmail.com>
+ */
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Layout from "../Layout";
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<Layout />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
